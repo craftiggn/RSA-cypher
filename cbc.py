13d0bb429af8720c8b42f0c82cf55215d86cebff
marcel_PC <craft.iggn@gmail.com>
2026-01-19 09:59:44 +0100

    added message to blocks (integers) conversion functionality, with variable block size and block padding

--- cbc.py
+++ cbc.py
@@ -10,6 +10,7 @@ such that any data type can be segmented and subbmitted to the algorithm
 
 # KEYGEN
 from Crypto.Util import number
+import random
 
 def random_prime(no_bits: int) -> int:
     """
@@ -61,5 +62,75 @@ def keygen(no_bits: int) -> tuple[int, int, int]:
 
     return e, d, n
 
-print(keygen(10))
-print(keygen(10))
+
+## CONVERTING THE DATA INTO BLOCKS
+
+def generate_iv(block_size: int) -> int:
+    min_val = 0
+    max_val = 2 ** (block_size * 8) - 1
+    return random.randint(min_val, max_val)
+
+def pad_message(message: bytes, block_size: int) -> bytes:
+    # PKCS-style padding
+    # NOTE can fail if the padding exceeds 255 (one byte)
+    padding_len = block_size - (len(message) % block_size)
+    return message + bytes([padding_len] *  padding_len) # adds n number n numbers at the end of the message 
+
+def unpad_message(message: bytes) -> bytes:
+    padding_len = message[-1]
+    return message[:-padding_len]
+
+def convert_msg_to_blocks(message: bytes, block_size: int) -> list[int]:
+    blocks = []
+    # pad the message
+    message = pad_message(message, block_size)
+
+    # split the message into blocks
+    for i in range(0, len(message), block_size):
+        # iterate over the message
+        block = message[i: i+block_size]
+        block_int = int.from_bytes(block, byteorder="big")
+        blocks.append(block_int)
+    
+    return blocks
+
+def string_to_blocks(text: str, block_size: int) -> list[int]:
+
+    # pad the message
+    print(f"text is:\n{text}")
+    message = text.encode("utf-8")
+    print(f"message encoded in utf-8 is:\n{message}")
+    message = pad_message(message, block_size)
+    print(f"padded message (block size = {block_size}) is:\n{message}")
+
+    # convert the message into blocks
+    blocks = []
+    for i in range(0, len(message), block_size):
+        # iterate over the message
+        block = message[i: i+block_size]
+        block_int = int.from_bytes(block, byteorder="big")
+        blocks.append(block_int)
+    print(f"message in block form is:\n{blocks}")
+    return blocks
+
+def blocks_to_string(blocks: list[int], block_size: int) -> str:
+
+    message = b""
+    for block in blocks:
+        chunk = block.to_bytes(block_size, byteorder="big")
+        message += chunk
+
+    message = unpad_message(message)
+    print(f"decoded message is:\n {message}")
+    return message.decode("utf-8")
+
+# testing of the padding functionality
+if True:
+    text = "Bicycle Day is an unofficial celebration on April 19th of the psychedelic revolution[1] and the first psychedelic trip on LSD by Albert Hofmann in 1943, in tandem with his bicycle ride home from Sandoz Labs.[2][3] It is commonly celebrated by ingesting psychedelics and riding a bike, sometimes in a parade,[4] and often with psychedelic-themed festivities.[5] The holiday was first named and declared in 1985 by Thomas Roberts, a psychology professor at Northern Illinois University,[6][7] but has likely been celebrated by psychedelic enthusiasts since the beginning of the psychedelic era, and celebrated in popular culture since at least 2004.[8]"
+
+    # text = "Bicycle Day (psychedelic holiday)"
+    block_size = 10
+
+    blocks = string_to_blocks(text, block_size)
+
+    decoded_text = blocks_to_string(blocks, block_size)
