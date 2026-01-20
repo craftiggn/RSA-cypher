a88ea84389f7025880193e72787c36cd41ef09e8
marcel_PC <craft.iggn@gmail.com>
2026-01-20 22:19:58 +0100

    Added docstirngs for my functions.

--- cbc.py
+++ cbc.py
@@ -1,23 +1,51 @@
-from Crypto.Util import number
 import random 
 import rsa_core
 
+# NOTE RSA is not a block cipher and CBC mode is not used in real-world cryptosystems.
 
-# RSA is not a block cipher and CBC mode is not used in real-world cryptosystems.
-# testing of the key generation algorithm
-
-
-# CONVERTING THE DATA INTO BLOCKS
+# ENCRYPTING THE DATA IN CBC MODE
 def generate_iv(block_size: int) -> int:
+    """
+    :param block_size: Block size in bytes.
+    :type block_size: int
+    :return: Initialisation vector "iv" used as c_0 in cipher-block chaining.
+    :rtype: int
+    """
     min_val = 0
     max_val = 2 ** (block_size * 8) - 1
     return random.randint(min_val, max_val)
 
 
 def xor_int(a: int, b: int) -> int:
+    """
+    Performs a bitwise XOR operation on two integers.
+
+    :param a: First operand.
+    :type a: int
+    :param b: Second operand.
+    :type b: int
+    :return: Result of a XOR b.
+    :rtype: int
+    """
     return a ^ b
 
 def rsa_cbc_encrypt(blocks: list[int], e: int, n: int, iv: int, block_size: int) -> list[int]:
+    """
+    Encrypts a list of blocks (integers) using RSA encryption in CBC mode. 
+    
+    :param blocks: Blocks to be encrypted.
+    :type blocks: list[int]
+    :param e: Public key.
+    :type e: int
+    :param n: RSA modulus.
+    :type n: int
+    :param iv: Initialisation vector.
+    :type iv: int
+    :param block_size: Size of blocks (in bytes).
+    :type block_size: int
+    :return: Encrypted blocks.
+    :rtype: list[int]
+    """
     encrypted_blocks = []
     mask = (1 << (block_size * 8)) - 1
 
@@ -27,12 +55,28 @@ def rsa_cbc_encrypt(blocks: list[int], e: int, n: int, iv: int, block_size: int)
         mixed = block ^ prev
         encrypted_block = rsa_core.rsa_encrypt_block(mixed, e, n)
         encrypted_blocks.append(encrypted_block)
-        prev = encrypted_block & mask # important
+        prev = encrypted_block & mask
 
     return encrypted_blocks
 
 
 def rsa_cbc_decrypt(encrypted_blocks: list[int], d: int, n: int, iv: int, block_size: int) -> list[int]:
+    """
+    Decrypts a list of blocks (integers) using RSA encryption in CBC mode. 
+    
+    :param encrypted_blocks: Blocks to be decrypted.
+    :type encrypted_blocks: list[int]
+    :param d: Private key.
+    :type d: int
+    :param n: RSA modulus.
+    :type n: int
+    :param iv: Initialisation vector.
+    :type iv: int
+    :param block_size: Size of blocks (in bytes).
+    :type block_size: int
+    :return: Decrypted blocks.
+    :rtype: list[int]
+    """
     blocks = []
     mask = (1 << (block_size * 8)) - 1
 
@@ -40,23 +84,53 @@ def rsa_cbc_decrypt(encrypted_blocks: list[int], d: int, n: int, iv: int, block_
 
     for encrypted_block in encrypted_blocks:
         mixed = rsa_core.rsa_decrypt_block(encrypted_block, d, n)
-        mixed &= mask # importatn
+        mixed &= mask 
         block = mixed ^ prev
         blocks.append(block)
-        prev = encrypted_block & mask # important
+        prev = encrypted_block & mask
 
     return blocks
 
 
-def encrypt_text(text: str, e: int, n: int, block_size: int):
+def encrypt_text(text: str, e: int, n: int, block_size: int) -> tuple[int, list[int]]:
+    """
+    Encrypts a given text using RSA encryption in CBC mode.
+    
+    :param text: Text to be encrypted.
+    :type text: str
+    :param e: Public key.
+    :type e: int
+    :param n: RSA modulus
+    :type n: int
+    :param block_size: Size of blocks (in bytes).
+    :type block_size: int
+    :return: Initialization vector and encrypted blocks.
+    :rtype: tuple[int, list[int]]
+    """
     rsa_core.validate_block_size(block_size, n)
     blocks = rsa_core.string_to_blocks(text, block_size)
     iv = generate_iv(block_size)
 
-    encrypted_blocks = rsa_cbc_encrypt(blocks, e, n, iv, block_size)  # FIX
+    encrypted_blocks = rsa_cbc_encrypt(blocks, e, n, iv, block_size)
     return iv, encrypted_blocks
 
 
-def decrypt_text(encrypted_blocks, d, n, iv, block_size):
+def decrypt_text(encrypted_blocks: list[int], d: int, n: int, iv: int, block_size: int) -> str:
+    """
+    Decrypts encrypted blocks using RSA encryption in CBC mode back into text.
+    
+    :param encrypted_blocks: Encrypted blocks.
+    :type encrypted_blocks: list[int]
+    :param d: Private key.
+    :type d: int
+    :param n: RSA modulus.
+    :type n: int
+    :param iv: Initialisation vector.
+    :type iv: int
+    :param block_size: Size of blocks (in bytes).
+    :type block_size: int
+    :return: Decrypted text.
+    :rtype: str
+    """
     blocks = rsa_cbc_decrypt(encrypted_blocks, d, n, iv, block_size)
     return rsa_core.blocks_to_string(blocks, block_size)
--- rsa_core.py
+++ rsa_core.py
@@ -1,12 +1,14 @@
 from Crypto.Util import number
-import random
 
 # KEYGEN
 def random_prime(no_bits: int) -> int:
     """
-    Docstring for random_prime
+    A function for generating primes of desired length.
+    Due to difficulty of this task in cryptography, an external library was used.\n
+    This library could be replaced by a bespoke function,
+    but it would be very inefficient if written in Python.
     
-    :param no_bits: Bit length of the desired prime.
+    :param no_bits: Bit length of the prime factors used to construct the RSA modulus.
     :type no_bits: int
     :return: random prime.
     :rtype: int
@@ -15,16 +17,14 @@ def random_prime(no_bits: int) -> int:
 
     return number.getPrime(no_bits)
 
-def is_prime(prime: int) -> bool:
-    return True
-
 def keygen(no_bits: int) -> tuple[int, int, int]:
     """
-    Docstring for keygen
+    Generates a private & public key of desired bit length, along with the RSA modulus,
+    to be used in RSA encryption.
     
-    :param no_bits: Description
+    :param no_bits: Bit length of private, public encryption key.
     :type no_bits: int
-    :return: Description
+    :return: public key, private key, RSA modulus.
     :rtype: tuple[int, int, int]
     """
     # get 2 distinct primes
@@ -52,22 +52,60 @@ def keygen(no_bits: int) -> tuple[int, int, int]:
 
     return e, d, n
 
+# CONVERTING THE DATA INTO BLOCKS
 def validate_block_size(block_size: int, n: int):
+    """
+    An important function, validating that the blocks are not too big for RSA modulus.\n
+    Throws a ValueError if the block size is too large.
+
+    :param block_size: Size of blocks (in bytes).
+    :type block_size: int
+    :param n: RSA modulus.
+    :type n: int
+    """
     if block_size * 8 >= n.bit_length():
         raise ValueError("Block size too large for RSA modulus")
 
 def pad_message(message: bytes, block_size: int) -> bytes:
+    """
+    Pads the message so that its length is a multiple of the block size.
+    
+    :param message: Message to be paded in byte form.
+    :type message: bytes
+    :param block_size: Desired block size for the padding.
+    :type block_size: int
+    :return: Paded message.
+    :rtype: bytes
+    """
     # PKCS-style padding
     # NOTE can fail if the padding exceeds 255 (one byte)
     padding_len = block_size - (len(message) % block_size)
-    return message + bytes([padding_len] *  padding_len) # adds n number n numbers at the end of the message 
+    return message + bytes([padding_len] *  padding_len) # appends padding_len bytes, each equal to padding_len
 
 
 def unpad_message(message: bytes) -> bytes:
+    """
+    Unpads the message.
+    
+    :param message: Message to be unpaded in byte form.
+    :type message: bytes
+    :return: Unpaded message.
+    :rtype: bytes
+    """
     padding_len = message[-1]
     return message[:-padding_len]
 
 def string_to_blocks(text: str, block_size: int) -> list[int]:
+    """
+    Converts a string to a list of integers representing message blocks, ready for encryption.
+    
+    :param text: Text to be divided into blocks.
+    :type text: str
+    :param block_size: Desired size of blocks (bytes).
+    :type block_size: int
+    :return: Text converted to a list of integers "blocks".
+    :rtype: list[int]
+    """
 
     # pad the message
     # print(f"text is:\n{text}")
@@ -87,7 +125,16 @@ def string_to_blocks(text: str, block_size: int) -> list[int]:
     return blocks
 
 def blocks_to_string(blocks: list[int], block_size: int) -> str:
-
+    """
+    Converts a list of integers (blocks) back into a single string.
+    
+    :param blocks: Blocks to be converted.
+    :type blocks: list[int]
+    :param block_size: Size of blocks.
+    :type block_size: int
+    :return: Converted text.
+    :rtype: str
+    """
     message = b""
     for block in blocks:
         chunk = block.to_bytes(block_size, byteorder="big")
@@ -97,11 +144,36 @@ def blocks_to_string(blocks: list[int], block_size: int) -> str:
     # print(f"decoded message is:\n {message}")
     return message.decode("utf-8")
 
+# Wiktor add your documentation here
 
 def rsa_encrypt_block(m: int, e: int, n: int) -> int:
+    """
+    Docstring for rsa_encrypt_block
+    
+    :param m: Description
+    :type m: int
+    :param e: Description
+    :type e: int
+    :param n: Description
+    :type n: int
+    :return: Description
+    :rtype: int
+    """
     if m >= n:
         raise ValueError("Block too large for modulus")
     return pow(m, e, n)
 
 def rsa_decrypt_block(c: int, d: int, n: int) -> int:
+    """
+    Docstring for rsa_decrypt_block
+    
+    :param c: Description
+    :type c: int
+    :param d: Description
+    :type d: int
+    :param n: Description
+    :type n: int
+    :return: Description
+    :rtype: int
+    """
     return pow(c, d, n)
